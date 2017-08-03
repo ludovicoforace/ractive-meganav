bb48beb7bc4aa937b0768d1b1a1ca70eb35b6ce5
Work <ludovicoforace@yahoo.com>
2017-08-03 23:00:29 +0100

    Add hederMob, move http request to main

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,4 @@
 import Ractive from 'ractive';
-import request from 'then-request';
 import template from './header.ract';
 import board from './header/board.js';
 import { targetNotContained } from '../targetNotContained.js';
@@ -12,32 +11,9 @@ export default Ractive.extend({
   data: () => ({
     activated: null,
     activatedSide: null,
-    showBoard: false,
-    mainHead: {},
-    snowflakes: {},
-    schopenhauer: {},
-    eagle: {},
-    physiognomy: {}
+    showBoard: false
   }),
   oninit() {
-    const self = this;
-    request('GET', './model.json',
-      {
-        headers: { accept: 'application/json' }
-      })
-      .then((res) => {
-        const data = JSON.parse(res.getBody());
-        self.set('mainHead', data.mainHead);
-        self.set('snowflakes', data.snowflakes);
-        self.set('schopenhauer', data.schopenhauer);
-        self.set('eagle', data.eagle);
-        self.set('physiognomy', data.physiognomy);
-      })
-      .catch((err) => {
-        alert(err);
-        console.log(err);
-      });
-
     this.on('active', (evt, i) => {
       if (targetNotContained(this)) {
         this.set('showBoard', true);
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Ractive from 'ractive';
+import request from 'then-request';
 import template from './main.ract';
 
 import preHeader from './components/preHeader.js';
 import header from './components/header.js';
+import headerMob from './components/headerMob.js';
 import tech from './components/tech.js';
 import footer from './components/footer.js';
 
@@ -12,8 +14,35 @@ const Main = new Ractive({
   components: {
     PreHeader: preHeader,
     Header: header,
+    HeaderMob: headerMob,
     Tech: tech,
     Footer: footer
+  },
+  data: () => ({
+    mainHead: {},
+    snowflakes: {},
+    schopenhauer: {},
+    eagle: {},
+    physiognomy: {}
+  }),
+  oninit() {
+    const self = this;
+    request('GET', './model.json',
+      {
+        headers: { accept: 'application/json' }
+      })
+      .then((res) => {
+        const data = JSON.parse(res.getBody());
+        self.set('mainHead', data.mainHead);
+        self.set('snowflakes', data.snowflakes);
+        self.set('schopenhauer', data.schopenhauer);
+        self.set('eagle', data.eagle);
+        self.set('physiognomy', data.physiognomy);
+      })
+      .catch((err) => {
+        alert(err);
+        console.log(err);
+      });
   }
 });
 
